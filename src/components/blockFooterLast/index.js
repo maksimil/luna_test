b617c6dd08e3ca09cafac081ea4725555111a06c
demig00d <28487425+demig00d@users.noreply.github.com>
2022-11-02 19:36:24 +0300

    Add links to footer

--- src/components/blockFooterLast/index.js
+++ src/components/blockFooterLast/index.js
@@ -20,22 +20,22 @@ export default function LastBlock(){
                             <br/>ИНН ??????? / ОГРН ?????????</p>
                     </div>
                     <div className={styles.text2}>
-                        <a href=""><h3>репертуар</h3></a>
-                        <a href=""><p>афиша</p></a>
-                        <a href=""><p>спектакли</p></a>
-                        <a href=""><p>маленькая луна</p></a>
+                        <a href="http://www.lunatheatre.ru/shows"><h3>репертуар</h3></a>
+                        <a href="http://www.lunatheatre.ru/afisha"><p>афиша</p></a>
+                        <a href="http://www.lunatheatre.ru/shows"><p>спектакли</p></a>
+                        <a href="http://www.lunatheatre.ru/pages/dtc-malenkaya-luna"><p>маленькая луна</p></a>
                     </div>
                     <div className={styles.text3}>
-                        <a href=""><h3>О «Луне» </h3></a>
-                        <a href=""><p>творческий состав</p></a>
-                        <a href=""><p>афиша</p></a>
-                        <a href=""><p>Спектакли</p></a>
-                        <a href=""><p>маленькая луна</p></a>
+                        <a href="http://www.lunatheatre.ru/pages/o-lune"><h3>О «Луне» </h3></a>
+                        <a href="http://www.lunatheatre.ru/actors"><p>творческий состав</p></a>
+                        <a href="http://www.lunatheatre.ru/afisha"><p>афиша</p></a>
+                        <a href="http://www.lunatheatre.ru/shows"><p>Спектакли</p></a>
+                        <a href="http://www.lunatheatre.ru/pages/dtc-malenkaya-luna"><p>маленькая луна</p></a>
                     </div>
                 </div>
                 <div className={styles.group2}>
                     <p>Театр Луны © Все права защищены. 1994–2022</p>
-                    <a href=""><p>Пользовательское соглашение</p></a>
+                    <a href="http://www.lunatheatre.ru/pages/polzovatelskoe-soglashenie"><p>Пользовательское соглашение</p></a>
                 </div>
             </div>
         </>
